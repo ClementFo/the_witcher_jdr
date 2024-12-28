de39a0a5e0a6d2d80684f6c7dcafe0ae75615fd5
clement <clementfornes@gmail.com>
2024-12-28 18:00:18 +0100

    Mise à jour de l'ajout des nouveaux composants d'artisanat

--- src/pages/ComposantArtisanat/index.jsx
+++ src/pages/ComposantArtisanat/index.jsx
@@ -1,4 +1,4 @@
-import { composant_artisanat } from '../../datas/composant_artisanat.js';
+import { materiaux, origine_animale, traitements_alchimiques, lingots_mineraux } from '../../datas/composant_artisanat.js';
 import '../../styles/App.css';
 import Accordeon from '../../components/Accordeon';
 
@@ -7,7 +7,14 @@ function ComposantArtisanat() {
     <div>
       <h2 className='title'>Composants d'artisanat</h2>
       <section>
-        <Accordeon DataList={composant_artisanat}></Accordeon>
+        <h3 className="title">Matériaux</h3>
+        <Accordeon DataList={materiaux}></Accordeon>
+        <h3 className="title">Peaux et composants d'origine animale</h3>
+        <Accordeon DataList={origine_animale}></Accordeon>
+        <h3 className="title">Traitements alchimique</h3>
+        <Accordeon DataList={traitements_alchimiques}></Accordeon>
+        <h3 className="title">Lingots et minéraux</h3>
+        <Accordeon DataList={lingots_mineraux}></Accordeon>
       </section>
     </div>
   );
