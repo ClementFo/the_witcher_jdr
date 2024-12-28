c6d9ead446b09e40973b367b68636457ec159d35
clement <clementfornes@gmail.com>
2024-12-28 17:59:11 +0100

    Ajout de nouveaux composants d'artisanat

--- src/datas/composant_artisanat.js
+++ src/datas/composant_artisanat.js
@@ -1,4 +1,4 @@
-export const composant_artisanat = [
+export const materiaux = [
     {
         name: 'Bois durci',
         category: 'Matériaux d\'artisanat',
@@ -128,5 +128,344 @@ export const composant_artisanat = [
         sd_research: "None",
         weight: '0,5 kg',
         price: 5
+    }
+]
+
+export const origine_animale = [
+    {
+        name: 'Cuir de dragonide',
+        category: 'Peaux et composants d\'origine animale',
+        rarety: 'Rare',
+        localisation: 'Acheté ou fabriqué',
+        quantity: '-',
+        sd_research: 'None',
+        weight: '5 kg',
+        price: 58
+    },
+    {
+        name: 'Cuir de vache',
+        category: 'Peaux et composants d\'origine animale',
+        rarety: 'Commune',
+        localisation: 'Acheté',
+        quantity: '-',
+        sd_research: 'None',
+        weight: '5 kg',
+        price: 10
+    },
+    {
+        name: 'Cuir durci',
+        category: 'Peaux et composants d\'origine animale',
+        rarety: 'Inhabituelle',
+        localisation: 'Acheté ou fabriqué',
+        quantity: '-',
+        sd_research: 'None',
+        weight: '3 kg',
+        price: 48
+    },
+    {
+        name: 'Cuir lyrien',
+        category: 'Peaux et composants d\'origine animale',
+        rarety: 'Inhabituelle',
+        localisation: 'Acheté ou fabriqué',
+        quantity: '-',
+        sd_research: 'None',
+        weight: '2 kg',
+        price: 60
+    },
+    {
+        name: 'Cuir',
+        category: 'Peaux et composants d\'origine animale',
+        rarety: 'Commune',
+        localisation: 'Acheté ou fabriqué',
+        quantity: '-',
+        sd_research: 'None',
+        weight: '2 kg',
+        price: 28
+    },
+    {
+        name: 'Ecaille de draconide',
+        category: 'Peaux et composants d\'origine animale',
+        rarety: 'Rare',
+        localisation: 'Wyverns',
+        quantity: '1d6',
+        sd_research: 'None',
+        weight: '5 kg',
+        price: 30
+    },
+    {
+        name: 'Os de bête',
+        category: 'Peaux et composants d\'origine animale',
+        rarety: 'Commune',
+        localisation: 'Monstres et bêtes',
+        quantity: 'Variable',
+        sd_research: 'None',
+        weight: '4 kg',
+        price: 8
+    },
+    {
+        name: 'Peau de loup',
+        category: 'Peaux et composants d\'origine animale',
+        rarety: 'Commune',
+        localisation: 'Loups',
+        quantity: '3 unités',
+        sd_research: 'None',
+        weight: '3 kg',
+        price: 14
+    },
+    {
+        name: 'Plumes',
+        category: 'Peaux et composants d\'origine animale',
+        rarety: 'Partout',
+        localisation: 'Oiseaux',
+        quantity: '1d6 unité(s)',
+        sd_research: 'None',
+        weight: '0,1 kg',
+        price: 4
+    }
+]
+
+export const traitements_alchimiques = [
+    {
+        name: 'Cinquième essence',
+        category: 'Traitements alchimique',
+        rarety: 'Rare',
+        localisation: 'Sites d\'énergie, mages et fiellons',
+        quantity: 'Variable',
+        sd_research: 'None',
+        weight: '0,1 kg',
+        price: 82
+    },
+    {
+        name: 'Cire d\'ogre',
+        category: 'Traitements alchimique',
+        rarety: 'Commune',
+        localisation: 'Grottes',
+        quantity: '1d10 unité(s)',
+        sd_research: 14,
+        weight: '0,1 kg',
+        price: 10
+    },
+    {
+        name: 'Eau-forte',
+        category: 'Traitements alchimique',
+        rarety: 'Commune',
+        localisation: 'Grottes et montagnes',
+        quantity: '1d10 unité(s)',
+        sd_research: 14,
+        weight: '0,1 kg',
+        price: 2
+    },
+    {
+        name: 'Graisse ester',
+        category: 'Traitements alchimique',
+        rarety: 'Commune',
+        localisation: 'Forêts et champs',
+        quantity: '1d10 unité(s)',
+        sd_research: 14,
+        weight: '0,1 kg',
+        price: 8
+    },
+    {
+        name: 'Herbe à tanner',
+        category: 'Traitements alchimique',
+        rarety: 'Commune',
+        localisation: 'Champs et forêts',
+        quantity: '1d10 unité(s)',
+        sd_research: 14,
+        weight: '0,1 kg',
+        price: 3
+    },
+    {
+        name: 'Huile assombrissante',
+        category: 'Traitements alchimique',
+        rarety: 'Inhabituelle',
+        localisation: 'Forêts',
+        quantity: '1d6 unité(s)',
+        sd_research: 16,
+        weight: '0,1 kg',
+        price: 24
+    },
+    {
+        name: 'Huile de dermoptère',
+        category: 'Traitements alchimique',
+        rarety: 'Inhabituelle',
+        localisation: 'Rivières et côtes',
+        quantity: '1d6 unité(s)',
+        sd_research: 16,
+        weight: '0,1 kg',
+        price: 45
+    },
+    {
+        name: 'Pierre à aiguiser',
+        category: 'Traitements alchimique',
+        rarety: 'Inhabituelle',
+        localisation: 'Montagnes et grottes',
+        quantity: '1d6 unité(s)',
+        sd_research: 16,
+        weight: '0,1 kg',
+        price: 32
+    }
+]
+
+export const lingots_mineraux = [
+    {
+        name: 'Acier Mahakamien',
+        category: 'Lingots et minéraux',
+        rarety: 'Inhabituelle',
+        localisation: 'Acheté ou fabriqué',
+        quantity: '-',
+        sd_research: 'None',
+        weight: '1 kg',
+        price: 114
+    },
+    {
+        name: 'Acier noir',
+        category: 'Lingots et minéraux',
+        rarety: 'Rare',
+        localisation: 'Acheté ou fabriqué',
+        quantity: '-',
+        sd_research: 'None',
+        weight: '1 kg',
+        price: 82
+    },
+    {
+        name: 'Acier tretogor',
+        category: 'Lingots et minéraux',
+        rarety: 'Inhabituelle',
+        localisation: 'Acheté ou fabriqué',
+        quantity: '-',
+        sd_research: 'None',
+        weight: '1 kg',
+        price: 64
+    },
+    {
+        name: 'Acier',
+        category: 'Lingots et minéraux',
+        rarety: 'Inhabituelle',
+        localisation: 'Acheté ou fabriqué',
+        quantity: '-',
+        sd_research: 'None',
+        weight: '1 kg',
+        price: 48
+    },
+    {
+        name: 'Argent',
+        category: 'Lingots et minéraux',
+        rarety: 'Rare',
+        localisation: 'Montagnes et sous-sols',
+        quantity: '1d6/2 unité(s)',
+        sd_research: 16,
+        weight: '1 kg',
+        price: 72
+    },
+    {
+        name: 'Argile alluviale',
+        category: 'Lingots et minéraux',
+        rarety: 'Inhabituelle',
+        localisation: 'Rivières et rivages',
+        quantity: '1d6 unité(s)',
+        sd_research: 14,
+        weight: '1,5 kg',
+        price: 5
+    },
+    {
+        name: 'Dimeritium mahakamien',
+        category: 'Lingots et minéraux',
+        rarety: 'Rare',
+        localisation: 'Acheté ou fabriqué',
+        quantity: '-',
+        sd_research: 'None',
+        weight: '1 kg',
+        price: 300
+    },
+    {
+        name: 'Dimeritium',
+        category: 'Lingots et minéraux',
+        rarety: 'Rare',
+        localisation: 'Acheté ou fabriqué',
+        quantity: '-',
+        sd_research: 'None',
+        weight: '1 kg',
+        price: 240
+    },
+    {
+        name: 'Fer noir',
+        category: 'Lingots et minéraux',
+        rarety: 'Rare',
+        localisation: 'Montagnes et sous-sols',
+        quantity: '1d6 unité(s)',
+        sd_research: 18,
+        weight: '1,5 kg',
+        price: 52
+    },
+    {
+        name: 'Fer',
+        category: 'Lingots et minéraux',
+        rarety: 'Inhabituelle',
+        localisation: 'Montagnes et sous-sols',
+        quantity: '1d6 unité(s)',
+        sd_research: 16,
+        weight: '1,5 kg',
+        price: 30
+    },
+    {
+        name: 'Gemme',
+        category: 'Lingots et minéraux',
+        rarety: 'Rare',
+        localisation: 'Montagnes et sous-sols',
+        quantity: '1d6/2 unité(s)',
+        sd_research: 24,
+        weight: '0,1 kg',
+        price: 100
+    },
+    {
+        name: 'Météorite',
+        category: 'Lingots et minéraux',
+        rarety: 'Rare',
+        localisation: 'Partout en extérieur',
+        quantity: '1d6/2 unité(s)',
+        sd_research: 24,
+        weight: '1 kg',
+        price: 98
+    },
+    {
+        name: 'Minerai luisant',
+        category: 'Lingots et minéraux',
+        rarety: 'Rare',
+        localisation: 'Montagnes et sous-sols',
+        quantity: '1d6/2 unité(s)',
+        sd_research: 20,
+        weight: '1 kg',
+        price: 80
+    },
+    {
+        name: 'Or',
+        category: 'Lingots et minéraux',
+        rarety: 'Rare',
+        localisation: 'Montagnes et sous-sols',
+        quantity: '1d6/2 unité(s)',
+        sd_research: 18,
+        weight: '1 kg',
+        price: 85
+    },
+    {
+        name: 'Pierre',
+        category: 'Lingots et minéraux',
+        rarety: 'Partout',
+        localisation: 'Montagnes et sous-sols',
+        quantity: '2d6 unités',
+        sd_research: 8,
+        weight: '2 kg',
+        price: 4
+    },
+    {
+        name: 'Poudre zerrikanienne',
+        category: 'Lingots et minéraux',
+        rarety: 'Inhabituelle',
+        localisation: 'Montagnes et sous-sols',
+        quantity: '1d6/2 unité(s)',
+        sd_research: 18,
+        weight: '0,1 kg',
+        price: 30
     }
 ]
