b2aa376731013a4aaba36cd1f17b2af3ae228efc
clement <clementfornes@gmail.com>
2025-04-14 17:46:42 +0200

    Mise à jour de la partie ListData

--- src/components/ListData/index.jsx
+++ src/components/ListData/index.jsx
@@ -3,7 +3,11 @@ import ListGroup from 'react-bootstrap/ListGroup';
 function ListData({ObjectData}) {
   const items = [];
   for (const [key, value] of Object.entries(ObjectData)) {
-    items.push(<ListGroup.Item key={key}>{key} : {value}</ListGroup.Item>);
+    if(key == "composant"){
+      items.push(<ListGroup.Item key={key}>{key} : {value.join(", ")}</ListGroup.Item>);
+    }else{
+      items.push(<ListGroup.Item key={key}>{key} : {value}</ListGroup.Item>);
+    }
   }
   return (
     <ListGroup>
