014cc5a7605456c7648ef7fd05d6d6d00a15efd6
clement <clementfornes@gmail.com>
2025-04-15 15:00:37 +0200

    Correction de problèmes

--- src/datas/race_jouable.js
+++ src/datas/race_jouable.js
@@ -1,50 +1,50 @@
 export const race_jouable = [
     {
         id: 1,
-        nom: "Sorceleur", 
-        particularité: ["Sens accrus", "Mutation durable", "Sensibilité émoussée", "Réflexes hors du commun"], 
-        bonus: ["+1 Vigilance", "+1 Reflexes", "+1 Dextérité", "-4 Empathie"]
+        nom: 'Sorceleur', 
+        particularité: ['Sens accrus', 'Mutation durable', 'Sensibilité émoussée', 'Réflexes hors du commun'], 
+        bonus: ['+1 Vigilance', '+1 Reflexes', '+1 Dextérité', '-4 Empathie']
     },
     {
         id: 2,
-        title: "Elfe", 
-        particularité: ["Esthète", "Oeil d\'aigle", "Harmonie avec la nature"], 
-        bonus: ["+1 Beaux-arts", "+2 Archerie"]
+        title: 'Elfe', 
+        particularité: ['Esthète', 'Oeil d\'aigle', 'Harmonie avec la nature'], 
+        bonus: ['+1 Beaux-arts', '+2 Archerie']
     },
     {
         id: 3,
-        title: "Nain", 
-        particularité: ["Tanné comme du cuir", "Coriace", "Oeil de l\'expert"], 
-        bonus: ["+2 Pouvoir d\'arrêt", "+1 Physique", "+25 Encombrement", "+1 Négoce"]
+        title: 'Nain', 
+        particularité: ['Tanné comme du cuir', 'Coriace', 'Oeil de l\'expert'], 
+        bonus: ['+2 Pouvoir d\'arrêt', '+1 Physique', '+25 Encombrement', '+1 Négoce']
     },
     {
         id: 4,
-        title: "Humain", 
-        particularité: ["Digne de confiance", "Ingénieux", "Tétu comme une mule"], 
-        bonus: ["+1 Charisme face à un humain", "+1 Séduction face à un humain", "+1 Persuasion face à un humain", "+1 Déduction"]
+        title: 'Humain', 
+        particularité: ['Digne de confiance', 'Ingénieux', 'Tétu comme une mule'], 
+        bonus: ['+1 Charisme face à un humain', '+1 Séduction face à un humain', '+1 Persuasion face à un humain', '+1 Déduction']
     },
     {
         id: 5,
-        title: "Gnome", 
-        particularité: ["De bonne compagnie", "Sens du détail", "Traque olfactive", "Petit gabarit"], 
-        bonus: ["+1 Charisme", "+2 à 3 compétences de technique", "+1 Vigilance", "-5 Physique", "3 COR pour encombrement et dégât corp-à-corp"]
+        title: 'Gnome', 
+        particularité: ['De bonne compagnie', 'Sens du détail', 'Traque olfactive', 'Petit gabarit'], 
+        bonus: ['+1 Charisme', '+2 à 3 compétences de technique', '+1 Vigilance', '-5 Physique', '3 COR pour encombrement et dégât corp-à-corp']
     },
     {
         id: 6,
-        title: "Vrans", 
-        particularité: ["Sang froid", "Griffes et crocs", "Peau écailleuse", "Physiologie reptilienne"], 
-        bonus: ["+1 Résistance à la contrainte", "Attaque avec croc à 3D6 et 50% empoisonnement", "+4 Pouvoir d\'arrêt", "-2 aux actions si gelé"]
+        title: 'Vrans', 
+        particularité: ['Sang froid', 'Griffes et crocs', 'Peau écailleuse', 'Physiologie reptilienne'], 
+        bonus: ['+1 Résistance à la contrainte', 'Attaque avec croc à 3D6 et 50% empoisonnement', '+4 Pouvoir d\'arrêt', '-2 aux actions si gelé']
     },
     {
         id: 7,
-        title: "Marmotins", 
-        particularité: ["Courageux comme des lions", "Physiologie singulière", "Dentition acérée", "Mauvaise vue"], 
-        bonus: ["+1 Courage", "Attaque avec croc à 2D6 et perforation améliorée", "-4 Vigilance"]
+        title: 'Marmotins', 
+        particularité: ['Courageux comme des lions', 'Physiologie singulière', 'Dentition acérée', 'Mauvaise vue'], 
+        bonus: ['+1 Courage', 'Attaque avec croc à 2D6 et perforation améliorée', '-4 Vigilance']
     },
     {
         id: 8,
-        title: "Halfelins", 
-        particularité: ["Agile", "Peuple agreste", "Résilience magique"], 
-        bonus: ["+1 Athlétisme", "+2 Survie", "+5 Résistance à la magie"]
+        title: 'Halfelins', 
+        particularité: ['Agile', 'Peuple agreste', 'Résilience magique'], 
+        bonus: ['+1 Athlétisme', '+2 Survie', '+5 Résistance à la magie']
     }
 ];
